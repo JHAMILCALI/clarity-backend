abb8aafc89e57c9ef2a6375d47857381bf524d56
jhamilcali <jhamilquea@gmail.com>
2025-10-23 19:43:53 -0400

    desplegado para deploy

--- app.py
+++ app.py
@@ -347,4 +347,5 @@ def check_transaction():
 # 🚀 Ejecutar servidor Flask
 # ==========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
